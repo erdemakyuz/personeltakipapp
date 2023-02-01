f7ab36adc3349bebacc727198b0dd260d7b82d34
Eyüp Can Dündar <eyup@ecdyazilim.com>
2023-02-01 09:37:03 +0300

    LoginScreen Düzenleme

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -10,6 +10,19 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        color: Colors.yellow,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(5),
+                child: TextField(),
+              )
+            ]),
+      ),
+    );
   }
 }
