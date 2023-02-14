a7e867b0c8e9829306c383386883061be395e8df
Eyüp Can Dündar <eyup@ecdyazilim.com>
2023-02-14 16:27:44 +0300

    QR Eklendi

--- lib/screens/QRScreen.dart
+++ lib/screens/QRScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:personeltakipapp/model/PersonelModel.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class QRScreen extends StatefulWidget {
   final PersonelModel? PersonelItem;
@@ -16,7 +17,17 @@ class _QRScreenState extends State<QRScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Personel QR")),
-      body: Container(color: Colors.yellow),
+      body: Container(
+        color: Colors.white,
+        child: Align(
+            alignment: Alignment.center,
+            child: QrImage(
+              data: this.PersonelItem?.TCKIMLIKNO ?? "",
+              version: QrVersions.auto,
+              size: 350,
+              foregroundColor: Colors.black,
+            )),
+      ),
     );
   }
 }
